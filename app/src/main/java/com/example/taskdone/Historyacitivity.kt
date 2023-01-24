60e60470b625a2807388c3c6121772db70e7abf8
Sujal <mishrasujal64@gmail.com>
2023-01-24 18:21:19 +0530

    Activity and xml Commit

---
+++ app/src/main/java/com/example/taskdone/Historyacitivity.kt
@@ -0,0 +1,11 @@
+package com.example.taskdone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Historyacitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historyacitivity)
+    }
+}
---
+++ app/src/main/java/com/example/taskdone/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.taskdone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.history->{
+                startActivity(Intent(this,Historyacitivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
